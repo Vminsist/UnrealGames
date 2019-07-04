53ca7ecf4309283cf56a9eba481b0c494b85c9ff
SmileChen518 <262000730@qq.com>
2019-07-05 01:39:56 +0800

    pinball

---
+++ PinBall/Source/PinBall/PinBallGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PinBallGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINBALL_API APinBallGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
