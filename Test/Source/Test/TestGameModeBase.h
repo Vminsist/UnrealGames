6ac497e097aef26b8a4273a5d690de03a8941772
SmileChen518 <262000730@qq.com>
2019-07-05 01:16:29 +0800

    test

---
+++ Test/Source/Test/TestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API ATestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
